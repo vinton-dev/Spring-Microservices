58c9c852d0a733dd3b4c6ef66fac3570d0745bf8
vinodt <vinodt@natsoft.us>
2021-02-26 12:51:24 +0530

    initial commit

---
+++ src/main/java/com/micro/demo/MainController.java
@@ -0,0 +1,13 @@
+package com.micro.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello";
+	}
+}
